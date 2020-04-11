37d926d05631616d9e9a70cba97ea2a75ebb55fd
crs <risheng17@gmail.com>
2020-04-11 16:32:04 +0800

    merge

--- ClinicalAppointmentRegistrationSystem-ejb/src/java/ejb/session/singleton/InitialiseDataSessionBean.java
+++ ClinicalAppointmentRegistrationSystem-ejb/src/java/ejb/session/singleton/InitialiseDataSessionBean.java
@@ -5,16 +5,24 @@
  */
 package ejb.session.singleton;
 
+import ejb.session.stateful.AppointmentEntitySessionBeanLocal;
 import ejb.session.stateless.DoctorEntitySessionBeanLocal;
+import ejb.session.stateless.PatientEntitySessionBeanLocal;
 import ejb.session.stateless.StaffEntitySessionBeanLocal;
+import entity.AppointmentEntity;
 import entity.DoctorEntity;
+import entity.PatientEntity;
 import entity.StaffEntity;
+import java.sql.Date;
+import java.sql.Time;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.ejb.LocalBean;
 import javax.ejb.Startup;
+import util.exception.AppointmentInvalidException;
 import util.exception.DoctorExistException;
+import util.exception.PatientExistException;
 import util.exception.StaffNotFoundException;
 import util.exception.StaffUsernameExistException;
 import util.security.EncryptionHelper;
@@ -33,7 +41,13 @@ public class InitialiseDataSessionBean {
     
     @EJB
     private DoctorEntitySessionBeanLocal doctorEntitySessionBeanLocal;
-
+    
+    @EJB
+    private PatientEntitySessionBeanLocal patientEntitySessionBeanLocal;
+    
+    @EJB
+    private AppointmentEntitySessionBeanLocal appointmentEntitySessionBeanLocal;
+    
     public InitialiseDataSessionBean() {
     }
     
@@ -53,13 +67,22 @@ public class InitialiseDataSessionBean {
         DoctorEntity doctorEntity2 = new DoctorEntity("Clair", "Hahn", "S41221", "MBBCh");
         DoctorEntity doctorEntity3 = new DoctorEntity("Robert", "Blake", "S58201", "MBBS");
         
+        hashPassword = EncryptionHelper.getInstance().byteArrayToHexString(EncryptionHelper.getInstance().doMD5Hashing("001001"));
+        PatientEntity patientEntity = new PatientEntity("Sarah", "Yi", "S9867027A", "F", 22, "93718799", "13, Clementi Road", hashPassword);
+        
+        AppointmentEntity appointmentEntity = new AppointmentEntity(Date.valueOf("2020-04-17"), Time.valueOf("10:00:00"));
+        appointmentEntity.setDoctorEntity(doctorEntity1);
+        appointmentEntity.setPatientEntity(patientEntity);
+        
         try {
             staffEntitySessionBeanLocal.addNewStaff(staffEntity);
-//            doctorEntitySessionBeanLocal.addNewDoctor(doctorEntity1);
-//            doctorEntitySessionBeanLocal.addNewDoctor(doctorEntity2);
-//            doctorEntitySessionBeanLocal.addNewDoctor(doctorEntity3);
+            doctorEntitySessionBeanLocal.addNewDoctor(doctorEntity1);
+            doctorEntitySessionBeanLocal.addNewDoctor(doctorEntity2);
+            doctorEntitySessionBeanLocal.addNewDoctor(doctorEntity3);
+            patientEntitySessionBeanLocal.addNewPatient(patientEntity);
+            appointmentEntitySessionBeanLocal.createNewAppointment(appointmentEntity);
             
-        } catch (StaffUsernameExistException ex) {
+        } catch (StaffUsernameExistException | DoctorExistException | PatientExistException | AppointmentInvalidException ex) {
             System.out.println(ex.getMessage() + "\n");
         }
     }
